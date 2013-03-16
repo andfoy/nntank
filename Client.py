238d9386cd34bea1c3811318f0e2a8914ad76cb6
andfoy <andfoy@gmail.com>
2013-03-15 20:56:22 -0500

    Added Multthread process

--- Client.py
+++ Client.py
@@ -5,6 +5,7 @@ import sys
 import serial
 import signal
 import pygame
+import threading
 from multiprocessing import Process
 import numpy as np
 import scipy
@@ -29,49 +30,92 @@ pygame.display.set_caption('Driver')
 ubuntu = pygame.font.match_font('Ubuntu')
 font = pygame.font.Font(ubuntu, 13)
 
+#global targets
+#global inputs
+#global status
 
 
-def getKeyPress():
-    while not status:
-          pygame.event.pump()
-          keys = pygame.key.get_pressed()
-          #print keys
-          targets, status = processOutputs(targets, keys)
-    return targets
+def main():
+    d = Driver()
+    d.initializeUI()
+    try:
+      join_threads(d.threads)
+    except KeyboardInterrupt:
+      print "\nKeyboardInterrupt catched."
+      print "Terminate main thread."
+      print "If only daemonic threads are left, terminate whole program."
+      
+class Driver(object):
+      targets = []
+      inputs = []
+      x = 0
+      status = False
+      def __init__(self):
+          self.running = True
+          self.threads = []
 
-def getImage():
-    x = 0
-    targets = []
-    inputs = []
-    while True:
-          urllib.urlretrieve("http://192.168.0.10:8080/shot.jpg", "input.jpg")
-          inputX = mpimg.imread('input.jpg')
-          inputX = rgb2gray(inputX)/255
-          out = inputX.copy()
-          out = scipy.misc.imresize(out, (352, 288), interp='bicubic', mode=None)
-          scipy.misc.imsave('input.png', out)
-          inputs = processImages(inputX, inputs)
-          #plt.imshow(inputX, cmap = plt.get_cmap('gray'))
-          #plt.ion()
-          #plt.show()
-          ###plt.draw()
-          img=pygame.image.load('input.png')
-          screen.blit(img,(0,0))
-          pygame.display.flip() # update the display
-          c.tick(3) # only three images per second
-          pygame.event.pump()
-          keys = pygame.key.get_pressed()
-          #print keys
-          targets, status = processOutputs(targets, keys)
-          if status:
-             break
-          else:
-             x += 1
-    targets = np.array(targets)
-    targets = flattenMatrix(targets)
-    sio.savemat('inputs.mat', {'inputs':inputs})
-    sio.savemat('targets.mat', {'targets':targets})
-    return inputs, targets
+      def getKeyPress(self):
+          while not self.status:
+                pygame.event.pump()
+                keys = pygame.key.get_pressed()
+                #print keys
+                self.targets, self.status = processOutputs(self.targets, keys)
+          if self.status:
+             self.running = False
+             self.targets = np.array(targets)
+             self.targets = flattenMatrix(targets)
+             sio.savemat('targets.mat', {'targets':self.targets})
+        
+      def getImage(self):
+          while True:
+                urllib.urlretrieve("http://192.168.0.10:8080/shot.jpg", "input.jpg")
+                inputX = mpimg.imread('input.jpg')
+                inputX = rgb2gray(inputX)/255
+                out = inputX.copy()
+                out = scipy.misc.imresize(out, (352, 288), interp='bicubic', mode=None)
+                scipy.misc.imsave('input.png', out)
+                inputs = processImages(inputX, self.inputs)
+                #plt.imshow(inputX, cmap = plt.get_cmap('gray'))
+                #plt.ion()
+                #plt.show()
+                ###plt.draw()
+                img = pygame.image.load('input.png')
+                screen.blit(img,(0,0))
+                pygame.display.flip() # update the display
+                c.tick(3) # only three images per second
+                #pygame.event.pump()
+                #keys = pygame.key.get_pressed()
+                #print keys
+                #targets, status = processOutputs(targets, keys)
+                if self.status:
+                   self.running = False
+                   sio.savemat('inputs.mat', {'inputs':self.inputs})
+                else:
+                   self.x += 1
+
+      def initializeUI(self):
+          t1 = threading.Thread(target=self.getImage)
+          t2 = threading.Thread(target=self.getKeyPress)
+          # Make threads daemonic, i.e. terminate them when main thread
+          # terminates. From: http://stackoverflow.com/a/3788243/145400
+          t1.daemon = True
+          t2.daemon = True
+          t1.start()
+          t2.start()
+          self.threads.append(t1)
+          self.threads.append(t2)
+
+
+def join_threads(threads):
+    """
+    Join threads in interruptable fashion.
+    From http://stackoverflow.com/a/9790882/145400
+    """
+    for t in threads:
+        while t.isAlive():
+            t.join(5)
+
+        
 
 def rgb2gray(rgb):
     r, g, b = np.rollaxis(rgb[...,:3], axis = -1)
@@ -139,4 +183,7 @@ def displayMode(mode):
     textRect.centerx = 20 #screen.get_rect().centerx
     textRect.centery = 20 #screen.get_rect().centery
     screen.blit(text, textRect)
-    pygame.display.update()   
+    pygame.display.update()
+
+if __name__ == "__main__":
+    main()   
